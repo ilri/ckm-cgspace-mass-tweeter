b0078c9d4cf99e7bae37540c311849356d0a495a
Tsegaselassie Tadesse <tsegaselassi@gmail.com>
2016-01-28 14:23:23 +0300

    Added the feature to edit community hash tags. Closes #15

--- client/views/pages/settings.js
+++ client/views/pages/settings.js
@@ -1,13 +1,25 @@
+editHashTag = new ReactiveVar();
+
 Template.settings.helpers({
   communities: function(){
     return Communities.find({}, {sort: {name: 1}});
+  },
+  editHashTagMode: function(){
+    return editHashTag.get() == this._id;
   }
 });
 
 Template.settings.events({
-
-});
-
-Template.settings.onRendered(function(){
-
+  "dblclick td.hashTag": function(e, t){
+    editHashTag.set(this._id);
+  },
+  "keyup input#edit-hash-tag": function(e, t){
+    var newHashTag = e.target.value;
+    if(e.keyCode == 13){ // Submit change
+      Communities.update({_id: this._id}, { $set: { hashTag: newHashTag }});
+      editHashTag.set(null);
+    } else if (e.keyCode == 27){ // Undo change
+      editHashTag.set(null);
+    }
+  }
 });
